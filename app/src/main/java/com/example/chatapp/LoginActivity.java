8262592249ee6490178533a68bd0d1c987bcd847
20120619 <mtuong669@gmail.com>
2022-10-14 21:56:08 +0700

    Done UI Register form https://mtuong669.atlassian.net/jira/software/projects/A2G/boards/2?selectedIssue=A2G-3

--- app/src/main/java/com/example/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.chatapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
-
+    TextView btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignup = (TextView) findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
