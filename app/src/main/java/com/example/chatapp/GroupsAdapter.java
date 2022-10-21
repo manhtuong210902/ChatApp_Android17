079099c15d4261f4fb5c1bf4f4066f4457014a68
20120619 <mtuong669@gmail.com>
2022-10-21 11:02:52 +0700

    Fix UI home chats and groups chat https://mtuong669.atlassian.net/jira/software/projects/A2G/boards/2?selectedIssue=A2G-9

--- app/src/main/java/com/example/chatapp/GroupsAdapter.java
+++ app/src/main/java/com/example/chatapp/GroupsAdapter.java
@@ -47,12 +47,10 @@ public class GroupsAdapter extends RecyclerView.Adapter<GroupsViewHolder>{
 }
 
 class GroupsViewHolder extends RecyclerView.ViewHolder{
-    CardView cardView_GroupItem;
     TextView textView_GroupName,  textView_GroupStatus;
     CircleImageView image_GroupAvatar;
     public GroupsViewHolder(@NonNull View itemView) {
         super(itemView);
-        cardView_GroupItem = itemView.findViewById(R.id.cardView_GroupItem);
         textView_GroupName = itemView.findViewById(R.id.textView_GroupName);
         textView_GroupStatus = itemView.findViewById(R.id.textView_GroupStatus);
         image_GroupAvatar = itemView.findViewById(R.id.image_GroupAvatar);
--- app/src/main/java/com/example/chatapp/RegisterActivity.java
+++ app/src/main/java/com/example/chatapp/RegisterActivity.java
@@ -1,15 +1,16 @@
 package com.example.chatapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
     }
 }
