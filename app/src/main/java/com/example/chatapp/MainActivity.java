a9c0975931e67fb975030d2b5f51dea2b91ed82c
tun2g <tunghcltt@gmail.com>
2022-10-27 14:19:00 +0700

    set splash screen https://mtuong669.atlassian.net/jira/software/projects/A2G/boards/2?selectedIssue=A2G-11

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.chatapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
-
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Demo login form UI
-        Intent intent = new Intent(MainActivity.this, AddGroupActivity.class);
-        startActivity(intent);
+        //splash screen
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, AddGroupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
     }
 }
