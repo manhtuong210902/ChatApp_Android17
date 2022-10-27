95de027cce5b55f003f0c21f487638500623a6cb
tun2g <tunghcltt@gmail.com>
2022-10-27 21:26:00 +0700

    fix miss file

--- app/src/main/java/com/example/chatapp/SplashActivity.java
+++ app/src/main/java/com/example/chatapp/SplashActivity.java
@@ -1,2 +1,21 @@
-package com.example.chatapp;public class SplashActivity {
+package com.example.chatapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    Handler handler=new Handler();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+        }},3000);
+    }
 }
