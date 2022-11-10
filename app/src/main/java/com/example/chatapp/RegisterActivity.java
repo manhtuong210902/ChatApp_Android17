66f54d4ad8c120518531c89226dd08b358b4bf4f
20120541 <nhiyenphan99@gmail.com>
2022-11-10 22:09:27 +0700

    Hoàn thành chức năng đăng nhập và đăng ký https://mtuong669.atlassian.net/jira/software/projects/A2G/boards/2?selectedIssue=A2G-16 https://mtuong669.atlassian.net/jira/software/projects/A2G/boards/2?selectedIssue=A2G-15

--- app/src/main/java/com/example/chatapp/RegisterActivity.java
+++ app/src/main/java/com/example/chatapp/RegisterActivity.java
@@ -3,15 +3,32 @@ package com.example.chatapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends Activity {
     EditText editText_emailRegister, editText_userNameRegister, editText_passwordRegister;
     TextView btn_login;
     Button btn_signup;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +40,9 @@ public class RegisterActivity extends Activity {
         btn_login = findViewById(R.id.btn_login);
         btn_signup = findViewById(R.id.btn_signup);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +50,53 @@ public class RegisterActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editText_emailRegister.getText().toString();
+                String username = editText_userNameRegister.getText().toString();
+                String password = editText_passwordRegister.getText().toString();
+                boolean check = true;
+                if(password.isEmpty() && password.length() <6){
+                    editText_passwordRegister.setError("Invalid password");
+                    check = false;
+                }
+                if(email.isEmpty() ){
+                    editText_emailRegister.setError("Invalid email");
+                    check = false;
+                }
+                if(username.isEmpty()){
+                    editText_userNameRegister.setError("Invalid email");
+                    check = false;
+                }
+                if(check)
+                    createUserWithEmailPass( );
+            }
+        });
     }
+
+    protected void createUserWithEmailPass(){
+        String email = editText_emailRegister.getText().toString();
+        String username = editText_userNameRegister.getText().toString();
+        String password = editText_passwordRegister.getText().toString();
+        if(email.isEmpty() || username.isEmpty() || password.isEmpty()){
+            Toast.makeText(RegisterActivity.this, "Thong tin dang ky khong hop le", Toast.LENGTH_LONG).show();
+        }
+        else{
+            mAuth.createUserWithEmailAndPassword( email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if( task.isSuccessful()){
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
 }
