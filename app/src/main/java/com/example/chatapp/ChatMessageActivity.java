17cd41fe738e8dccd54269b81f6a80ef51bc4d3e
tun2g <tunghcltt@gmail.com>
2022-11-16 16:34:38 +0700

    add

--- app/src/main/java/com/example/chatapp/ChatMessageActivity.java
+++ app/src/main/java/com/example/chatapp/ChatMessageActivity.java
@@ -13,6 +13,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +24,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ChatMessageActivity extends Activity {
     private ImageView btnSend;
@@ -30,7 +34,7 @@ public class ChatMessageActivity extends Activity {
 
     private ArrayList<ChatMessage> listChat;
     private ChatMessageAdapter adapter;
-
+    private FirebaseAuth mAuth;
     DatabaseReference databaseReference;
 
     @Override
@@ -53,29 +57,17 @@ public class ChatMessageActivity extends Activity {
             @Override
             public void onClick(View view) {
                 String message = etInputMessage.getText().toString();
-                ChatMessage chat = new ChatMessage("21:05", message, "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true);
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                String currentTime = sdf.format(new Date());
+
+                ChatMessage chat = new ChatMessage(currentTime.toString(), message, "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true);
                 sendMessage(chat , idGroup);
-                Toast.makeText(ChatMessageActivity.this, "clickSend", Toast.LENGTH_SHORT).show();
                 etInputMessage.setText("");
             }
         });
 
         listChat = new ArrayList<>();
-//        listChat.add(new ChatMessage("20:22", "Hello", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao toi la nguyen manh tuong hom nay chung ta se den vs chanel cua tuong", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao toi la nguyen manh tuong hom nay chung ta se den vs chanel cua tuong", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", false));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao ban", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", false));
-//        listChat.add(new ChatMessage("20:22", "Yes Sua", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "OkeLuon", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao ban", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", false));
-//        listChat.add(new ChatMessage("20:22", "Hello", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao toi la nguyen manh tuong hom nay chung ta se den vs chanel cua tuong", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao toi la nguyen manh tuong hom nay chung ta se den vs chanel cua tuong", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", false));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao ban", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", false));
-//        listChat.add(new ChatMessage("20:22", "Yes Sua", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "OkeLuon", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", true));
-//        listChat.add(new ChatMessage("20:22", "Xin Chao ban", "ddVfY1n6kEN2UK0sdX8wBKFn0Mg1", false));
-//        readMessage(idGroup);
+
         databaseReference = FirebaseDatabase.getInstance().getReference("Groups").child(idGroup);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
