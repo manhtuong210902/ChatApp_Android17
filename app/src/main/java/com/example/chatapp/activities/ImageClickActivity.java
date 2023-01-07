823190e3468efd930e613755db61aa990c4a3edd
tun2g <tunghcltt@gmail.com>
2023-01-08 03:28:09 +0700

    photo view

---
+++ app/src/main/java/com/example/chatapp/activities/ImageClickActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageClickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_click);
+    }
+}
